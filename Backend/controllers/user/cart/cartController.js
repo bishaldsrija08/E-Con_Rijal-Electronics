149d9266edef6e23b1a93d9df1daa2c424282857
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-01-28 23:05:51 +0545

    feat(cart): Add remove and update product functionality in cart

--- Backend/controllers/user/cart/cartController.js
+++ Backend/controllers/user/cart/cartController.js
@@ -26,7 +26,7 @@ exports.addToCart = async (req, res) => {
     res.status(200).json({ message: "Product added to cart successfully.", cart: user.cart });
 }
 
-exports.getMyCartItems = async(req, res)=>{
+exports.getMyCartItems = async (req, res) => {
     const userId = req.user.id;
 
     const user = await User.findById(userId).populate({
@@ -34,6 +34,36 @@ exports.getMyCartItems = async(req, res)=>{
         select: "-productStatus, -createdAt, -updatedAt, -__v"
     })
     const cart = user.cart
+}
 
 
+exports.removeFromCart = async (req, res) => {
+    const userId = req.user.id;
+    const { productId } = req.params;
+
+    if (!productId) {
+        return res.status(400).json({ message: "Product ID is required." });
+    }
+
+    //get user cart
+    const user = await User.findById(userId)
+    user.cart = user.cart.filter(item => item != productId)
+    await user.save()
+    res.status(200).json({ message: "Product removed from cart successfully.", cart: user.cart });
+}
+
+exports.updateProductInCart = async (req, res) => {
+    const userId = req.user.id;
+    const { productId } = req.params;
+
+    if (!productId) {
+        return res.status(400).json({ message: "Product ID is required." });
+    }
+
+    //get user cart
+    const user = await User.findById(userId)
+    user.cart = user.cart.filter(item => item != productId) // remove existing product
+    user.cart.push(productId) // add updated product
+    await user.save()
+    res.status(200).json({ message: "Product updated in cart successfully.", cart: user.cart });
 }
--- Backend/routes/cart/cartRoutes.js
+++ Backend/routes/cart/cartRoutes.js
@@ -1,4 +1,4 @@
-const { addToCart } = require('../../controllers/user/cart/cartController')
+const { addToCart, removeFromCart } = require('../../controllers/user/cart/cartController')
 const checkRole = require('../../middleware/checkRole')
 const isAuthenticated = require('../../middleware/isAuthenticated')
 const catchAsync = require('../../services/catchAsync')
@@ -6,6 +6,7 @@ const catchAsync = require('../../services/catchAsync')
 const router = require('express').Router()
 
 router.route("/cart").post(isAuthenticated, checkRole("customer"), catchAsync(addToCart))
+router.route("/cart/:productId").delete(isAuthenticated, checkRole("customer"), catchAsync(removeFromCart))
 
 
 module.exports = router
